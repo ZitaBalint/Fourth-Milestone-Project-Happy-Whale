9d6ae0e03fe7d8a1af6a29b9c24fe58a4e7c2369
ZitaBalint <zita.balint1072@gmail.com>
2021-08-15 21:51:16 +0000

    costumised admin

--- shop/admin.py
+++ shop/admin.py
@@ -2,5 +2,24 @@ from django.contrib import admin
 from .models import Item, Category
 
 
-admin.site.register(Item)
-admin.site.register(Category)
+class ItemAdmin(admin.ModelAdmin):
+    list_display = (
+        'tem_code',
+        'name',
+        'category',
+        'price',
+        'review_rating',
+        'image_url',
+    )
+
+    ordering = ('item_code', )
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'display_name',
+        'name',
+    )
+
+
+admin.site.register(Item, ItemAdmin)
+admin.site.register(Category, CategoryAdmin)
