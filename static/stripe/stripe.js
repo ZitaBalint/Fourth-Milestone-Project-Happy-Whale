607aeed5fb67ed27fa4b96d217278019aeb7047a
ZitaBalint <zita.balint1072@gmail.com>
2021-11-30 08:51:39 +0000

    chenged the checkout url

--- static/stripe/stripe.js
+++ static/stripe/stripe.js
@@ -45,7 +45,7 @@ var unit_total = document.getElementById("total").textContent;
 
 $.ajax({
     type: "POST",
-    url: "{% url 'checkout' %}",
+    url: '/checkout/',
     data: {
       title: title,
       first_name: firstName,
