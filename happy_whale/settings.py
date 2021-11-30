495ab2a78140e653d98d8741b9f077c0af509692
ZitaBalint <zita.balint1072@gmail.com>
2021-11-30 02:00:34 +0000

    deplaoyed part1

--- happy_whale/settings.py
+++ happy_whale/settings.py
@@ -153,16 +153,13 @@ WSGI_APPLICATION = 'happy_whale.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# if 'DATABASE_URL' in os.environ:
-#     DATABASES = {
-#         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-#     }
+
 # else:
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+   'default': {
+       'ENGINE': 'django.db.backends.sqlite3',
+    'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+   }
 }
 
 
