f04234cf4aeccc65e9b889f6f7a096673a4f5e5f
ZitaBalint <zita.balint1072@gmail.com>
2021-11-30 02:11:49 +0000

    sett debug

--- happy_whale/settings.py
+++ happy_whale/settings.py
@@ -29,8 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'TRUE'
-# 'DEVELOPMENT' in os.environ
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['happy-whale-milestone-project.herokuapp.com', 'localhost']
 
@@ -153,14 +152,17 @@ WSGI_APPLICATION = 'happy_whale.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-
-# else:
-DATABASES = {
-   'default': {
-       'ENGINE': 'django.db.backends.sqlite3',
-    'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-   }
-}
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }  
+    }
 
 
 # Password validation
